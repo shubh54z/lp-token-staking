faa5913a6cb4ccd02734542078e6043cff0ac220
shubh54z <71586369+shubh54z@users.noreply.github.com>
2021-09-22 17:10:50 +0530

    Update RewardsInfo.js

--- client/components/RewardsInfo.js
+++ client/components/RewardsInfo.js
@@ -7,14 +7,14 @@ export default ({ rewardRate, totalRewardsPaid }) => (
         <Form.Label column  style={{minWidth:250}} className="text-start">Rewards Rate</Form.Label>
         <Col />
         <Form.Label column  style={{minWidth:200}} className="text-end">
-          {rewardRate * 60 * 60 * 24} ETB / day
+          {rewardRate * 60 * 60 * 24} TRX / day
         </Form.Label>
       </Form.Group>
-      <Form.Group as={Row} controlId="lpTokenBalance">
+      <Form.Group as={Row} controlId="WHXTokenBalance">
         <Form.Label column  style={{minWidth:250}} className="text-start">Total Rewards Paid</Form.Label>
         <Col />
         <Form.Label column  style={{minWidth:200}} className="text-end">
-          {totalRewardsPaid} ETB
+          {totalRewardsPaid} TRX
         </Form.Label>
       </Form.Group>
   </Container>
